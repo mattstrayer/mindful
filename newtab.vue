bc08694929d060fa96812fc56f5d3b980df98c80
Matt Strayer <git@mattstrayer.com>
2024-01-07 17:33:36 -0500

    Update newtab.vue

--- newtab.vue
+++ newtab.vue
@@ -5,22 +5,26 @@ import "./styles.css"
 </script>
 
 <template>
-  <div class="dark bg-zinc-900 container">
-    <h2 class="text-center">
-      Welcome to your
-      <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
+  <div class="container">
+    <h2 class="text-zinc-100 text-4xl text-center">
+      be...mindful
     </h2>
   </div>
 </template>
 
 <style>
 
+#__plasmo {
+  display: flex;
+  justify-content: center;
+  width:100%;
+  height:100%;
 
+}
 .container {
   width:100%;
   height:100%;
 }
-.text-center {
-  text-align: center;
-}
+
+
 </style>
