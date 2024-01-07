4b69b77f165356a9b3946430518421b11d3768aa
Matt Strayer <git@mattstrayer.com>
2024-01-06 23:09:31 -0500

    add newtab page

---
+++ newtab.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div>
+    <h2 class="text-center">
+      Welcome to your
+      <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
+    </h2>
+  </div>
+</template>
+
+<style>
+.container {
+  min-width: 470px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 47px;
+}
+.text-center {
+  text-align: center;
+}
+</style>
