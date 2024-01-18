98acb4af56ad893f5664e445a8f196ee0125cadf
Matt Strayer <git@mattstrayer.com>
2024-01-18 12:37:00 -0500

    add grouped input

--- src/newtab.vue
+++ src/newtab.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-
-
 import "./index.css"
-
-
 </script>
 
 <template>
   <div class="container">
-    <button class="btn" >Hello!</Button>
-
+    <div class="join">
+      <input
+        type="text"
+        placeholder="Type here"
+        class="input input-bordered join-item w-full max-w-xs" />
+      <button class="btn btn-primary join-item">Hello!</button>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer">
+        <input type="checkbox" checked="checked" class="checkbox" />
+        <span class="label-text">Remember me</span>
+      </label>
+    </div>
 
     <h2 class="text-zinc-100 text-4xl text-center font-extrabold">
       be...mindful
@@ -18,18 +25,15 @@ import "./index.css"
 </template>
 
 <style>
-
 #__plasmo {
   display: flex;
   justify-content: center;
-  width:100%;
-  height:100%;
-
+  width: 100%;
+  height: 100%;
 }
+
 .container {
-  width:100%;
-  height:100%;
+  width: 100%;
+  height: 100%;
 }
-
-
 </style>
