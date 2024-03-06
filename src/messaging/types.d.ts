8ce9fb3dbdec943a9ffa62b588a0999555c78003
Matt Strayer <git@mattstrayer.com>
2024-03-06 17:54:38 -0500

    broadcast channel types

---
+++ src/messaging/types.d.ts
@@ -0,0 +1,16 @@
+
+
+declare type MessageHandler = (message: Message) => void
+
+declare type MessageTypes = "BLOCK_ENABLED"
+declare interface Message  {
+  type: MessageTypes;
+  data: any;
+};
+
+
+declare interface BlockEnabled extends Message {
+  type: "BLOCK_ENABLED";
+  data: boolean
+}
+
