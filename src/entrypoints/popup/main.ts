6142e24d96b514adbc3b4e73976bae905680a074
Matt Strayer <git@mattstrayer.com>
2024-03-11 20:30:30 -0400

    Update main.ts

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -3,11 +3,10 @@ import { createApp } from "vue"
 import "@/index.css"
 
 import { createPinia } from "pinia"
-import { createORM } from "pinia-orm"
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 
 import App from "./App.vue"
 
-const pinia = createPinia().use(createORM()).use(piniaPluginPersistedstate)
+const pinia = createPinia().use(piniaPluginPersistedstate)
 
 createApp(App).use(pinia).mount("#app")
